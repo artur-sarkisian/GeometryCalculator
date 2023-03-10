bce07b26818e482f9bf795b57fffa56a5119a23f
artursarkisyan <sai_7@mail.ru>
2023-03-10 11:46:20 +0300

    Initial commit

---
+++ src/main/java/practice/GeometryCalculator.java
@@ -0,0 +1,26 @@
+package practice;
+
+import net.sf.saxon.expr.Component;
+
+public class GeometryCalculator {
+
+    // если значение radius меньше 0, метод должен вернуть -1
+    public static double getCircleSquare(double radius) {
+        return 0.0;
+    }
+
+    // если значение radius меньше 0, метод должен вернуть -1
+    public static double getSphereVolume(double radius) {
+        return 0.0;
+    }
+
+    public static boolean isTrianglePossible(double a, double b, double c) {
+        return false;
+    }
+
+    // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
+    // методом isTrianglePossible, если невозможен вернуть -1.0
+    public static double getTriangleSquare(double a, double b, double c) {
+        return 0.0;
+    }
+}
